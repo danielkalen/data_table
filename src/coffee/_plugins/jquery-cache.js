e4df98b93dd74f245524dc84cfa8e46e7d3ca3ea
danielkalen <d@danielkalen.com>
2016-11-15 12:21:37 -0500

    [Remove] old _plugins folder

--- src/coffee/_plugins/jquery-cache.js
+++
@@ -1,2 +0,0 @@
-// jQuery Selector Cache
-!function(n,r){var $={};$$=function(u){var t=$[u];return t!==r?t:$[u]=n(u)},$$.clear=function(n){$[n]=r},$$.fresh=function(n){return $[n]=r,$$(n)}}(jQuery);
